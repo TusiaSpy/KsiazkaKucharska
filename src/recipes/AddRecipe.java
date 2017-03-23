e09f011a5eadae3795f038ba3860b27ff4ba84d4
TusiaSpy <walendowska.marta@gmail.com>
2017-03-23 23:36:33 +0100

    desery

--- src/recipes/AddRecipe.java
+++ src/recipes/AddRecipe.java
@@ -12,7 +12,7 @@ public class AddRecipe {
 	private Scanner s;
 
 	public List<Meal> add() throws FileNotFoundException{
-		File plik = new File("przepisy.txt");
+		File plik = new File("desery.txt");
 		s = new Scanner(plik);
 		s.useDelimiter(";");
 		while (s.hasNextLine()) {
@@ -25,12 +25,24 @@ public class AddRecipe {
 			int preparationTime = s.nextInt();
 			int numberOfPeople = s.nextInt();
 			String difficulty = s.next();
-			String ingredients = s.next();
+			String[] ingredients = s.next().split("/");
 			String preparation = s.next();
 
 			if (typ.equals("Deser")) {
 				String calories = s.next();
-				meals.add(new Dessert(preparationTime, numberOfPeople, preparation, difficulty, calories));
+				Dessert dessert = new Dessert(preparationTime, numberOfPeople, preparation, difficulty, calories);
+				//for
+				//Maka 2, Jajka 5,...
+				for ( int i=0; i<ingredients.length ; i++){
+					String[] productQantity = ingredients[i].split(" ",2); 
+					//maka, 2
+					int ilosc = Integer.parseInt(productQantity[1]);
+					dessert.addIngredient(productQantity[0], ilosc);
+					
+					
+				}
+				dessert.addIngredient(ingredients[0], 0);
+						meals.add(dessert);
 
 			} else if (typ.equals("Zupa")) {
 				boolean wegeMax = Boolean.getBoolean(s.next());
